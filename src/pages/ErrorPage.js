7b594456d86f60e99a960784c307f69a3eadf2a4
ugthefluffster <ugthefluffster@gmail.com>
2023-06-05 14:16:49 +0300

    good

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function ErrorPage() {
   return (
-    <div>
+    <div className='text-center'>
       An error has occured
       <div>
         <Link to="/">Back to the start...</Link>
